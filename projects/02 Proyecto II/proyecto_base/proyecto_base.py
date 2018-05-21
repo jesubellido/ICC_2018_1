11e1342aa5d909a1d6137669dfea8897f5a8abfa
jesubellido <jesubellido@gmail.com>
2018-05-21 16:23:15 -0500

    Proyecto Base 2

---
+++ projects/02 Proyecto II/proyecto_base/proyecto_base.py
@@ -0,0 +1,12 @@
+import instaintro_gui
+
+
+def tarea():
+	# Aqui empieza tu tarea
+	pass
+
+
+app = instaintro_gui.Application("tarea")
+app.title('InstaIntro')
+app.loadProgram(tarea)
+app.start()
